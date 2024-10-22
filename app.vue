22f63d30d27fb941d660bf362d7d62b790d437f7
Jerome Thayananthajothy <tjthavarshan@gmail.com>
2024-10-22 03:01:07 +0100

    Add initial project files and configurations

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="antialiased leading-normal font-sans flex items-center justify-center min-h-screen bg-gray-100">
+    <div class="bg-white p-10 rounded-xl shadow-lg text-center max-w-sm prose">
+      <img class="w-24 h-24 rounded-full mx-auto mb-4" src="/images/profile.png" alt="Paavalan Varathanathan">
+      <p class="text-lg font-semibold text-gray-800">Hi there! 👋</p>
+      <p class="text-gray-500">My name is <NuxtLink to="https://www.linkedin.com/in/paavalan-varathanathan-212951312" class="font-medium underline text-gray-800">Paavalan</NuxtLink>, and I'm a Software Engineer.</p>
+      <p class="text-gray-500">I'm still working on my personal website so stay tuned for great things to come!</p>
+      <img class="w-96 mx-auto mt-4" src="/images/giphy.gif" alt="Working Hard">
+    </div>
+  </div>
+</template>
